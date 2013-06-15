6305a9c8d60b2ba4ffefde30513b85a3829518de
kimvais <kimvais@ssh.com>
2013-06-15 10:24:44 +0300

    Added a simple tool to create users from the command line

---
+++ adduser.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Nimismies adduser tool
+Usage: adduser.py <email> [<dn>]
+"""
+import getpass
+import docopt
+
+from nimismies.models import User
+
+if __name__ == '__main__':
+    args = docopt.docopt(__doc__)
+    email = args.pop('<email>')
+    dn = args.pop('<dn>', None)
+    try:
+        u = User.objects.get(email=email)
+    except User.DoesNotExist:
+        u = None
+    if u is not None:
+        raise RuntimeError('User <{0}> already exists'.format(email))
+    if dn is None:
+        dn = 'CN={0}/emailAddress={1}'.format(getpass.getuser(), email)
+    password = getpass.getpass()
+    u = User.objects.create_user(email, dn)
+    u.set_password(password)
+    u.save()
+
+
